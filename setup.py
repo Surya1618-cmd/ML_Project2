1d00a6e951eae2ec2534e8975a125b2b77176149
Surya1618-cmd <suryayedugani@gmail.com>
2025-08-22 15:32:46 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup, find packages
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+from setuptools import setup,find_packages
+with open ("requirements.txt") as f:
+    requirements =f.read().splitlines()
 
 setup(
-    name="Personal Health Dashboard for Disease Prediction",
+    name="Personal Health Dashboard for Disease Prediction ",
     version="0.1",
     author="surya",
     packages=find_packages(),
-    install_requires=requirements
+    install_requires =requirements,
+
 )
